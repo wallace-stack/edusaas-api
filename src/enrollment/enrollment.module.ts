ae6449b9de389421e419c79a8587fb8459018ea6
wallace-stack <wallace.araujo05@outlook.com>
2026-03-26 18:11:32 -0300

    fix: add SchoolsModule to EnrollmentModule imports

--- src/enrollment/enrollment.module.ts
+++ src/enrollment/enrollment.module.ts
@@ -4,9 +4,13 @@ import { Enrollment } from './enrollment.entity';
 import { EnrollmentService } from './enrollment.service';
 import { EnrollmentController } from './enrollment.controller';
 import { User } from '../users/user.entity';
+import { SchoolsModule } from '../schools/schools.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Enrollment, User])],
+  imports: [
+    TypeOrmModule.forFeature([Enrollment, User]),
+    SchoolsModule,
+  ],
   controllers: [EnrollmentController],
   providers: [EnrollmentService],
   exports: [EnrollmentService],

abe8b8d322ab7db10e286660748e4c4904d4eb21
wallace-stack <wallace.araujo05@outlook.com>
2026-03-26 17:56:19 -0300

    fix: add SchoolsModule to FeedModule imports

--- src/feed/feed.module.ts
+++ src/feed/feed.module.ts
@@ -3,20 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { FeedService } from './feed.service';
 import { FeedController } from './feed.controller';
 import { FeedPost } from './feed-post.entity';
+import { SchoolsModule } from '../schools/schools.module'; 
 import { CloudinaryModule } from '../cloudinary/cloudinary.module';
-import { ClassesModule } from '../classes/classes.module';
-import { PlansModule } from '../plans/plans.module';
-import { EnrollmentModule } from '../enrollment/enrollment.module';
-import { SchoolsModule } from '../schools/schools.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([FeedPost]),
+    SchoolsModule, 
     CloudinaryModule,
-    ClassesModule,
-    PlansModule,
-    EnrollmentModule,
-    SchoolsModule,
   ],
   controllers: [FeedController],
   providers: [FeedService],

3b773f1bb946bd71eb57608c964df2af228d2409
wallace-stack <wallace.araujo05@outlook.com>
2026-06-29 23:33:50 -0300

    fix(perms): tornar PermissionsModule global (resolve DI do PermissionGuard)

--- src/permissions/permissions.module.ts
+++ src/permissions/permissions.module.ts
@@ -1,10 +1,11 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Global, Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserPermission } from './user-permission.entity';
 import { PermissionsService } from './permissions.service';
 import { PermissionsController } from './permissions.controller';
 import { UsersModule } from '../users/users.module';
 
+@Global()
 @Module({
   imports: [
     TypeOrmModule.forFeature([UserPermission]),
